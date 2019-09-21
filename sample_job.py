256c1acb7ef3778fab09493827df7b97a1e329d6
Nathan Killoran <co9olguy@users.noreply.github.com>
2019-09-21 16:11:29 -0400

    update sample job

--- sample_job.py
+++ sample_job.py
@@ -16,12 +16,41 @@ sample_circuit =   {
                                 "gate": "cnot",
                                 "target": 2,
                                 "control": 0
+                            },
+                            {
+                                "gate": "cnot",
+                                "target": 1,
+                                "control": 0
+                            },
+                            {
+                                "gate": "rx",
+                                "target": 0,
+                                "rotation": 0.421412
                             }
                         ]
                     }
+
+bell_circuit = \
+    {
+      "qubits": 2,
+      "circuit": [
+        {
+          "gate": "h",
+          "target": 0
+        },
+        {
+          "gate": "cnot",
+          "control": 0,
+          "target": 1
+        }
+      ]
+    }
+
 
 sample_job = {"lang": "json",
               "body": sample_circuit}
 
 job = Job()
-job.manager.create(body=sample_circuit, lang="json")
+job.manager.create(**sample_job)
+job.manager.get(job.id.value)
+print(job.data)
