69219907601703bbcf1ecc2a9d2f71cc874a5059
Josh Izaac <josh146@gmail.com>
2021-04-14 23:14:55 +0800

    Update pennylane_ionq/device.py
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>

--- pennylane_ionq/device.py
+++ pennylane_ionq/device.py
@@ -161,6 +161,7 @@ class IonQDevice(QubitDevice):
         # dict[str, float], and maps the computational basis
         # state (as a base-10 integer string) to the probability
         # as a floating point value between 0 and 1.
+        # e.g., {"0": 0.413, "9": 0.111, "17": 0.476}
         self.histogram = job.data.value["histogram"]
 
     @property
