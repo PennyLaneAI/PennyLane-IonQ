8e3c0c289e9593fd877e0492274a01a745321ee1
Spencer Churchill <churchill@ionq.co>
2023-10-26 08:58:31 -0700

    add custom backends to qpu class

--- pennylane_ionq/device.py
+++ pennylane_ionq/device.py
@@ -93,9 +93,13 @@ class IonQDevice(QubitDevice):
     # and therefore does not support the Hermitian observable.
     observables = {"PauliX", "PauliY", "PauliZ", "Hadamard", "Identity"}
 
-    def __init__(self, wires, *, target="simulator", gateset="qis", shots=1024, api_key=None):
+    def __init__(
+        self, wires, *, target="simulator", gateset="qis", shots=1024, api_key=None
+    ):
         if shots is None:
-            raise ValueError("The ionq device does not support analytic expectation values.")
+            raise ValueError(
+                "The ionq device does not support analytic expectation values."
+            )
 
         super().__init__(wires=wires, shots=shots)
         self.target = target
@@ -134,7 +138,9 @@ class IonQDevice(QubitDevice):
         rotations = kwargs.pop("rotations", [])
 
         if len(operations) == 0 and len(rotations) == 0:
-            warnings.warn("Circuit is empty. Empty circuits return failures. Submitting anyway.")
+            warnings.warn(
+                "Circuit is empty. Empty circuits return failures. Submitting anyway."
+            )
 
         for i, operation in enumerate(operations):
             if i > 0 and operation.name in {"BasisState", "QubitStateVector"}:
@@ -210,7 +216,8 @@ class IonQDevice(QubitDevice):
             # Here, we rearrange the states to match the big-endian ordering
             # expected by PennyLane.
             basis_states = (
-                int(bin(int(k))[2:].rjust(self.num_wires, "0")[::-1], 2) for k in self.histogram
+                int(bin(int(k))[2:].rjust(self.num_wires, "0")[::-1], 2)
+                for k in self.histogram
             )
             idx = np.fromiter(basis_states, dtype=int)
 
@@ -230,7 +237,9 @@ class IonQDevice(QubitDevice):
         if shot_range is None and bin_size is None:
             return self.marginal_prob(self.prob, wires)
 
-        return self.estimate_probability(wires=wires, shot_range=shot_range, bin_size=bin_size)
+        return self.estimate_probability(
+            wires=wires, shot_range=shot_range, bin_size=bin_size
+        )
 
 
 class SimulatorDevice(IonQDevice):
@@ -251,8 +260,12 @@ class SimulatorDevice(IonQDevice):
     name = "IonQ Simulator PennyLane plugin"
     short_name = "ionq.simulator"
 
-    def __init__(self, wires, *, target="simulator", gateset="qis", shots=1024, api_key=None):
-        super().__init__(wires=wires, target=target, gateset=gateset, shots=shots, api_key=api_key)
+    def __init__(
+        self, wires, *, target="simulator", gateset="qis", shots=1024, api_key=None
+    ):
+        super().__init__(
+            wires=wires, target=target, gateset=gateset, shots=shots, api_key=api_key
+        )
 
     def generate_samples(self):
         """Generates samples by random sampling with the probabilities returned by the simulator."""
@@ -279,8 +292,20 @@ class QPUDevice(IonQDevice):
     name = "IonQ QPU PennyLane plugin"
     short_name = "ionq.qpu"
 
-    def __init__(self, wires, *, target="qpu", gateset="qis", shots=1024, api_key=None):
-        super().__init__(wires=wires, target=target, gateset=gateset, shots=shots, api_key=api_key)
+    def __init__(
+        self,
+        wires,
+        *,
+        target="qpu",
+        backend="",
+        gateset="qis",
+        shots=1024,
+        api_key=None,
+    ):
+        target += backend
+        super().__init__(
+            wires=wires, target=target, gateset=gateset, shots=shots, api_key=api_key
+        )
 
     def generate_samples(self):
         """Generates samples from the qpu.
