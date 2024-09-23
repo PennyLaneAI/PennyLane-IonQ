00ad29ce1b6f492107d708adc50f99f75fc75890
Spencer Churchill <churchill@ionq.co>
2024-09-23 09:40:07 -0700

    add default to docs + set self.backend

--- pennylane_ionq/device.py
+++ pennylane_ionq/device.py
@@ -311,6 +311,7 @@ class QPUDevice(IonQDevice):
             or strings (``['ancilla', 'q1', 'q2']``).
         gateset (str): the target gateset, either ``"qis"`` or ``"native"``. Defaults to ``qis``.
         backend (str): Optional specifier for an IonQ backend. Can be ``"aria-1"``, ``"forte-1"``, etc.
+            Defaults to ``"aria-1"``.
         shots (int, list[int]): Number of circuit evaluations/random samples used to estimate
             expectation values of observables. Defaults to 1024. If a list of integers is passed, the
             circuit evaluations are batched over the list of shots.
@@ -341,9 +342,8 @@ class QPUDevice(IonQDevice):
         sharpen=None,
         api_key=None,
     ):
-        target = "qpu"
-        if backend is not None:
-            target += "." + backend
+        self.backend = backend
+        target = f"qpu.{self.backend}"
         super().__init__(
             wires=wires,
             target=target,
