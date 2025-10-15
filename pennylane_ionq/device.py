fc0c8f55c8febc6c8a3e4ad85194243cd5f44af6
andrijapau <andrija.paurevic@xanadu.ai>
2025-10-15 09:46:44 -0400

    minor fix

--- pennylane_ionq/device.py
+++ pennylane_ionq/device.py
@@ -352,7 +352,7 @@ class IonQDevice(QubitDevice):
             gate["terms"] = terms
             # 1. Float conversion to prevent numpy types (np.float64) in the JSON payload.
             # 2. IonQ API expects positive time values for their `pauliexp` gate.
-            time_sign = np.sign(float(operation.param))
+            time_sign = np.sign(operation.param)
             gate["time"] = abs(float(operation.param))
             # 3. Add missing sign convention to coefficients by multiplying by sign of time.
             gate["coefficients"] = [float(v * time_sign) for v in coefficients]
